5731b2ac57a219cabb5d47550a1efad7b71ad05e
Steven-Awo <rotimiawomodu@gmail.com>
2023-09-05 17:48:34 +0100

    TASK 6: a program that prints all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for numb1 in range(0, 10):
+    for numb2 in range(numb1 + 1, 10):
+        if numb1 == 8 and numb2 == 9:
+            print(f"{numb1}{numb2}")
+        else:
+            print(f"{numb1}{numb2}", end=", ")
