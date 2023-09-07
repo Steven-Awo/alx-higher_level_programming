e669eea8deb87432ae470f84f9a9a8024318bf29
Steven-Awo <rotimiawomodu@gmail.com>
2023-09-07 19:45:03 +0100

    TASK 2: Write a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,11 @@ if __name__ == "__main__":
     import sys
 
     countz = len(sys.argv) - 1
-
     if countz == 0:
         print("0 arguments.")
-
     elif countz == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(countz))
-    
-    for x in range(countz):
-        print("{}: {}".format(x + 1, sys.argv[x + 1]))
+    for i in range(countz):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
