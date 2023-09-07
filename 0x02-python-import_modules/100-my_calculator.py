f7b79d39ec100d63b17dfee15d3772e3462058f3
Steven-Awo <rotimiawomodu@gmail.com>
2023-09-07 21:39:27 +0100

    TASK 6: Write a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    
-    from calculator_1 import div, mul, sub, add
+
+    from calculator_1 import add, sub, mul, div 
     import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    optrs = {"/": div, "*": mul, "+": add, "-": sub}
-
-    if sys.argv[2] not in list(optrs.keys()):
+    oprs = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(oprs.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, optrs[sys.argv[2]](a, b)))
+    x = int(sys.argv[1])
+    y = int(sys.argv[3])
+    print("{} {} {} = {}".format(x, sys.argv[2], y, oprs[sys.argv[2]](x, y)))
