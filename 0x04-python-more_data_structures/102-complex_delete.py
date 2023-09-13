3206957a4aa52905dcdddd61d813f3ff306e1ddb
Steven-Awo <rotimiawomodu@gmail.com>
2023-09-13 20:32:19 +0100

    Task 15: Write a function that deletes keys with a specific value in a dictionary.

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,6 @@
 def complex_delete(a_dictionary, value):
     list_of_keys = list(a_dictionary.keys())
     for value_dicry in list_of_keys:
-        if val == a_dictionary.get(value_dicry):
+        if value == a_dictionary.get(value_dicry):
             del a_dictionary[value_dicry]
     return (a_dictionary)
