a957c6de679986aa139cfe6dc3ac6949c0708aec
Steven-Awo <rotimiawomodu@gmail.com>
2023-10-10 16:29:26 +0100

    TASK 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+"""Defining a function called file-writing."""
+
+
+def write_file(filename="", text=""):
+    """Writing a string to the UTF8 text file.
+
+    Args:
+        filename (str): The filename to write to.
+        text (str): The text to be written to the file.
+    Returns:
+        The number of the characters that was written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
