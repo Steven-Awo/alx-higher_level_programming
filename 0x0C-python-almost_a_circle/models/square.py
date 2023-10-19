ca7f77a1d4bd87a952a88d3e12fbdd883903ac8e
Steven-Awo <rotimiawomodu@gmail.com>
2023-10-19 14:58:48 +0100

    Square.PY

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Defining the class called Square class.'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Representing of the square class using rectangle class.'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Initializing a new size, x, y and id with super.'''
+        super().__init__(id, size, size, x, y)
+    
+    def __str__(self):
+        '''Returning the info about the string as a string.'''
+        return '[{}] ({}) {}/{} - {}/{}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.height)
