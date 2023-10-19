bbce3230a13df35a078798fda299b17028426a94
Steven-Awo <rotimiawomodu@gmail.com>
2023-10-19 11:40:30 +0100

    BASE.PY

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""Module for the class base model."""
+'''Module for Base class.'''
+
 
 class Base:
-    """the Base model.
-    Attributes:
-        __nb_object (int): Number of instantiated Bases.
-    """
+    '''A representation of the base of our OOP hierarchy.'''
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialization of the neww Base.
-
-        Args:
-            id (int): The neww base identity.
-        """
+        '''Constructor.'''
         if id is not None:
             self.id = id
         else:
