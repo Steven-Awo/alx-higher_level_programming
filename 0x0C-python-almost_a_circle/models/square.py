57ab149d44ed6336a1ffdf1523c8c9e098cd8072
Steven-Awo <rotimiawomodu@gmail.com>
2023-10-19 16:18:33 +0100

    Square.PY

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,7 +4,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''A Square class.'''
+    '''Representing of the square class class.'''
 
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor.'''
