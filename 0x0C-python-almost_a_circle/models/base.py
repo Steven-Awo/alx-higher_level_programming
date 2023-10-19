c8438cffe15d9577843f9ee0f3293d4044756914
Steven-Awo <rotimiawomodu@gmail.com>
2023-10-19 11:47:59 +0100

    BASE.PY

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,12 +4,11 @@
 
 class Base:
     '''Representation of a base of our own OOP hierarchy.'''
-
     __nb_objects = 0
 
     def __init__(self, id=None):
         '''Initializing a new id.'''
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
