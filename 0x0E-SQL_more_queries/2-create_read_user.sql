e1063ca3272e8933fbe91df0ebd8f63e5d2b26b2
Steven-Awo <rotimiawomodu@gmail.com>
2023-11-15 18:39:01 +0100

    TASK 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creates a database for hbtn_0d_2 and for the user user_0d_2
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a new user hbtn_0d_2
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants all the SELECT the privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
