160e51b6d3f79abf96f980828c8ae67a1ea03d97
Steven-Awo <rotimiawomodu@gmail.com>
2023-11-15 21:47:37 +0100

    TASK 20

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- listing all the genres in the database called hbtn_0d_tvshows_rate by their ratings
+-- listing all thr rows in the database thats linked to a row to another table
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
