af5237a8e7bc13ce85e6871693f48cc186c764d4
StevenAwo <rotimiawomodu@gmail.com>
2023-12-11 13:02:55 +0100

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+if (processed.argv.length === 2) {
+	console.log('No argument');
+}
+
+else if (processed.argv.length === 3) {
+	console.log('Argument found');
+}
+
+else {
+	console.log('Arguments found');
+}
