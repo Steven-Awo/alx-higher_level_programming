ba7e1336f24d8c15008f8c98ace49241696ec020
StevenAwo <rotimiawomodu@gmail.com>
2023-12-12 12:35:36 +0100

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-
   let nOccurrences = 0;
   for (let x = 0; x < list.length; x++) {
     if (searchElement === list[x]) {
