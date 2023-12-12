b3df328b9134e9c0077106e4cae82f1bca6b5086
StevenAwo <rotimiawomodu@gmail.com>
2023-12-12 12:01:27 +0100

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+class Rectangle {
+	constructor(w, h) {
+		if ((h > 0) && (w > 0)) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+
+	print () {
+		for (let x = 0; x < this.height; x++) {
+			let a = '';
+			for (let y = 0; y < this.width; y++) {
+				a += 'X';
+			}
+			console.log(a);
+		}
+	}
+}
+
+module.exports = Rectangle;
