9920a461a7712008b45806b2044fb25cb74eb375
StevenAwo <rotimiawomodu@gmail.com>
2023-12-12 18:20:48 +0100

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor (w, h) {
-		if ((h > 0) && (w > 0)) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if ((h > 0) && (w > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 
 module.exports = Rectangle;
