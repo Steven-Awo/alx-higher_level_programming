2aac1a667b90bf6c009a045287f8332ce586b32e
StevenAwo <rotimiawomodu@gmail.com>
2023-12-15 17:16:52 +0100

    TASK 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,13 @@ import sys
 
 
 if __name__ == "__main__":
-    dtb = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    dtb = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
     currt = dtb.cursor()
     currt.execute("SELECT * FROM states")
     rowws = currt.fetchall()
