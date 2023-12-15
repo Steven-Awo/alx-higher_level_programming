a53cf473bc5688c1e9655eae7e01b74ae53760e5
StevenAwo <rotimiawomodu@gmail.com>
2023-12-15 18:41:40 +0100

    TASK 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  lists all the states from the database called hbtn_0e_0_usa """
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    dtb = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    currt = dtb.cursor()
+    match = sys.argv[4]
+    currt.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rowws = currt.fetchall()
+    for one_row in rowws:
+        print(one_row)
+    currt.close()
+    dtb.close()
