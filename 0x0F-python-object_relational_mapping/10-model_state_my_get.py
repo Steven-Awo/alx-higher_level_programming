7d6eccf1aeba072f5bce15c716dd13fa9a05ccad
StevenAwo <rotimiawomodu@gmail.com>
2023-12-15 21:13:02 +0100

    TASK 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     enginee = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(enginee)
-    Session = sessionmaker(bind=enginee)
-    sessioner = Session()
+    Sessionr = sessionmaker(bind=enginee)
+    sessioner = Sessionr()
     instancer = sessioner.query(State).filter(State.name == (sys.argv[4],))
     try:
         print(instancer[0].id)
