1c2096d178e38632423a54d97987206c2a9b0040
StevenAwo <rotimiawomodu@gmail.com>
2023-12-15 18:31:49 +0100

    TASK 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,8 +5,13 @@ import sys
 
 
 if __name__ == "__main__":
-    dtb = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    dtb = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
     currt = dtb.cursor()
     currt.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                 .format(sys.argv[4]))
