4652b2e20d49168daa4f423d7ca2862e4b7b04fe
StevenAwo <rotimiawomodu@gmail.com>
2024-01-05 18:51:28 +0100

    TASK 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request and displays the size of the body of the response
-
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
+# A script that just takes in an URL, sends a request and then displays its  body's size of the response
+curl -sI "$1" | awk "Content-Length:/ {print $2}" | tr -d "\r\n "
