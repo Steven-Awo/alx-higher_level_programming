12251eab5b5e550561d330e0e3b44d733950a5b6
StevenAwo <rotimiawomodu@gmail.com>
2024-01-06 21:06:52 +0100

    TASK 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,6 +11,5 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    rqts = requests.post(sys.argv[1],
-            data={'email': sys.argv[2]})
+    rqts = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(rqts.text)
