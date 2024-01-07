43201ce9803a3fca97c51f925736c8e0cf922c81
StevenAwo <rotimiawomodu@gmail.com>
2024-01-07 21:20:48 +0100

    TASK 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,14 +13,12 @@ if __name__ == "__main__":
     letterr = "" if len(sys.argv) == 1 else sys.argv[1]
     payyloadr = {"q": letterr}
 
-    rqts = requests.post("http://0.0.0.0:5000/search_user",
-            data=payyloadr)
+    rqts = requests.post("http://0.0.0.0:5000/search_user", data=payyloadr)
     try:
         responsesr = rqts.json()
         if responsesr == {}:
             print("No result")
         else:
-            print("[{}] {}".format(responsesr.get("id"),
-                responsesr.get("name")))
+            print("[{}] {}".format(responsesr.get("id"), responsesr.get("name")))
     except ValueError:
         print("Not a valid JSON")
