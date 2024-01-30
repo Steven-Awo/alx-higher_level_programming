ce800a57401b07325efaacfdd6177b5292126a8e
StevenAwo <rotimiawomodu@gmail.com>
2024-01-30 22:49:07 +0100

    TASK 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const request = require('request');
+const requestt = require('request');
 
-request(process.argv[2], function (error, response, body) {
+requestt(process.argv[2], function (error, response, body) {
   if (!error) {
-    const results = JSON.parse(body).results;
-
-    console.log(results.reduce((count, movie) => {
+    const resultts = JSON.parse(body).resultts;
+    console.log(resultts.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
