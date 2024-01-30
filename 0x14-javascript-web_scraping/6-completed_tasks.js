751a06a311d907b9acedae64e33a1d33c68bf2ba
StevenAwo <rotimiawomodu@gmail.com>
2024-01-30 23:05:29 +0100

    TASK 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,25 +2,25 @@
 
 const requst = require('request');
 
-const urrl = process.argv[2];
+const urll = process.argv[2];
 
-requst(urrl, function (err, response, body) {
+requst(urll, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    const complleted = {};
-    const taskss = JSON.parse(body);
-    for (const x in taskss) {
-      const tassk = taskss[x];
-      if (tassk.complleted === true) {
-        if (complleted[tassk.userId] === undefined) {
-          complleted[tassk.userId] = 1;
+    const complteted = {};
+    const tassks = JSON.parse(body);
+    for (const x in tassks) {
+      const taskk = tassks[x];
+      if (taskk.complteted === true) {
+        if (complteted[taskk.userId] === undefined) {
+          complteted[taskk.userId] = 1;
         } else {
-          complleted[tassk.userId]++;
+          complteted[taskk.userId]++;
         }
       }
     }
-    console.log(complleted);
+    console.log(complteted);
   } else {
     console.log('An error occured. Status code: ' + response.statusCode);
   }
