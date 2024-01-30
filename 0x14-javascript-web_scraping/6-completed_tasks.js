8c8e4386658200b8cb67a087da129ef90291352e
StevenAwo <rotimiawomodu@gmail.com>
2024-01-30 23:17:38 +0100

    TASK 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
 const req = require('request');
-
 const id = process.argv[2];
-
 const url = 'https://swapi-api.hbtn.io/api/films/';
-
 req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
@@ -22,4 +19,3 @@ req.get(url + id, function (error, res, body) {
     });
   }
 });
-
