06c8d717cb909ab5440986405e9b1638ced3600e
StevenAwo <rotimiawomodu@gmail.com>
2024-01-30 23:38:00 +0100

    TASK 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 
-const reqt = require('request');
+const requst = require('request');
 
 const urll = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-reqt(urll, function (error, response, body) {
+requst(urll, function (error, response, body) {
   if (!error) {
-    const chtrs = JSON.parse(body).chtrs;
+    const chtrs = JSON.parse(body).characters;
     printCharacters(chtrs, 0);
   }
 });
 
-function printCharacters (chtrs, indx) {
-  reqt(chtrs[indx], function (error, response, body) {
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
-      if (indx + 1 < chtrs.length) {
-        printCharacters(chtrs, indx + 1);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
     }
   });
