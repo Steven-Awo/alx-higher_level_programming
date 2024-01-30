16cc97e09ab70a4ec564a1a3d103fa480747ef85
StevenAwo <rotimiawomodu@gmail.com>
2024-01-30 22:36:11 +0100

    TASK 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,9 +6,9 @@ const episodeNum = process.argv[2];
 
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL + episodeNum, function (errr, response, body) {
-  if (errr) {
-    console.log(errr);
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
   }
   else if (response.statusCode === 200) {
     const responseJSON = JSON.parse(body);
@@ -18,4 +18,3 @@ request(API_URL + episodeNum, function (errr, response, body) {
     console.log('Error code: ' + response.statusCode);
   }
 });
-
