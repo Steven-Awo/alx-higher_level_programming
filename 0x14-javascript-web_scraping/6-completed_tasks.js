2d5f7f13cef8e9ff9b86f03297a968d4b177db14
StevenAwo <rotimiawomodu@gmail.com>
2024-01-30 23:31:14 +0100

    TASK 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
+const urll = process.argv[2];
 
-request(url, function (err, response, body) {
+request(urll, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
