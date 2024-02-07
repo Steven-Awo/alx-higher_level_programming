fccc196af34c0098de59186301d8f8260249df47
StevenAwo <rotimiawomodu@gmail.com>
2024-02-07 16:35:36 +0100

    TASK 103

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	$("INPUT#btn_translate").click(fetchTranslation);
+	$("INPUT#language_code").keyup(function (event) {
+		if (event.keyCode === 13) {
+			fetchTranslation();
+		}
+	});
+	function fetchTranslation() {
+		const lang_code = $("INPUT#language_code").val();
+		$.getJSON(`https://fourtonfish.com/hellosalut/hello/?lang=${language_code}`,
+			function (data) {
+				$("#hello").text(data.hello);
+			}
+		);
+	}
+});
